337aaabc741ea43aacd128e6087e97400670c7e5
Ahmed Yehia <ahmedyehia2715@gmail.com>
2026-04-24 20:44:26 +0300

    dashboard

--- src/pages/Dashboard/DashBoard.tsx
+++ src/pages/Dashboard/DashBoard.tsx
@@ -1,8 +1,14 @@
-
-
+import StatBox from "../../components/StatBox";
 
 function DashBoard() {
-  return <div>dashBoard</div>;
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-4 gap-6 p-7">
+      <StatBox />
+      <StatBox />
+      <StatBox />
+      <StatBox />
+    </div>
+  );
 }
 
 export default DashBoard;
