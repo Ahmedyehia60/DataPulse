4e1f11427a32bf5ac89faf038ebf92b9c6f79c98
Ahmed Yehia <ahmedyehia2715@gmail.com>
2026-04-25 20:26:39 +0300

    order page

--- src/pages/Orders/Orders.tsx
+++ src/pages/Orders/Orders.tsx
@@ -1,23 +1,28 @@
 import { Download } from "lucide-react";
 
-function Orders() {
+const Orders = () => {
   return (
-    <div className="p-4">
-      <div>
-        <p className="text-3xl font-bold ">Order History</p>
-        <p className="text-sm mt-1 ml-1 text-[#717182]">
-          Track and manage customer orders.
-        </p>
-        <div className="w-40 h-15 shadow-2xl rounded-xl">
-          
-        <p>export</p>
-          
-          <Download />
-
+    <div className="flex flex-col gap-8">
+      <div className="flex flex-col md:flex-row md:items-center justify-between gap-4">
+        <div>
+          <h1 className="text-3xl font-bold text-gray-900">Order History</h1>
+          <p className="text-sm mt-1 text-gray-500">
+            Track and manage customer orders.
+          </p>
         </div>
+
+        <button className="flex items-center justify-center gap-2 px-4 py-2.5 cursor-pointer bg-white border border-gray-200 text-gray-700 rounded-xl shadow-sm hover:bg-gray-50 transition-all active:scale-95 group">
+          <Download
+            size={20}
+            className="text-gray-500 group-hover:text-blue-600"
+          />
+          <span className="font-semibold text-sm ">Export CSV</span>
+        </button>
       </div>
+
+    
     </div>
   );
-}
+};
 
 export default Orders;
