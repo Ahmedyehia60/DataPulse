da0e5af8736e0f3c8962301788fa8dd4b8f6d089
Ahmed Yehia <ahmedyehia2715@gmail.com>
2026-07-01 18:17:45 +0300

    updated in ui

--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -1,6 +1,7 @@
 import { Bell, Menu } from "lucide-react";
 import { useEffect, useRef, useState } from "react";
 import PopUp from "../components/PopUp";
+import Alert from "../components/Alert";
 
 const Navbar = ({ toggleSidebar }: { toggleSidebar: () => void }) => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -30,20 +31,11 @@ const Navbar = ({ toggleSidebar }: { toggleSidebar: () => void }) => {
         >
           <Menu size={28} />
         </button>
+
+        <Alert />
       </div>
 
       <div className="flex items-center gap-4 md:gap-6">
-        <div ref={bellRef} className="relative">
-          <div
-            className="cursor-pointer relative"
-            onClick={() => setIsOpen(!isOpen)}
-          >
-            <Bell className="text-gray-600" />
-            <span className="absolute -top-1 -right-1 w-2 h-2 bg-red-500 rounded-full"></span>
-          </div>
-
-          {isOpen && <PopUp onClose={() => setIsOpen(false)} />}
-        </div>
 
         <div className="flex items-center gap-3 border-l pl-4 border-gray-200">
           <div className="text-right">
